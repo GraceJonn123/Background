9a5d7df63ae9acf1bf56681fd2b7349605d5fbb5
GraceJonn123 <gj.gjonn@gmail.com>
2016-04-15 09:54:07 +0300

    adding play audio activity

--- app/src/main/java/com/jijjy/grace/background/PlayAudio.java
+++ app/src/main/java/com/jijjy/grace/background/PlayAudio.java
@@ -1,7 +1,49 @@
 package com.jijjy.grace.background;
 
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.util.Log;
+
 /**
  * Created by grace on 4/15/16.
  */
-public class PlayAudio {
+public class PlayAudio extends Service{
+    private static final String LOGCAT = null;
+    MediaPlayer objPlayer;
+
+    public void onCreate(){
+        super.onCreate();
+        Log.d(LOGCAT, "Service Started!");
+        objPlayer = MediaPlayer.create(this, R.raw.sleepaway);
+    }
+    public int onStartCommand(Intent intent, int flags, int startId){
+        objPlayer.start();
+        Log.d(LOGCAT, "Media player Started!");
+        if (objPlayer.isLooping() != true){
+            Log.d(LOGCAT, "Problem in PLaying Audio");
+        }
+        return 1;
+    }
+
+    public void onStop(){
+        objPlayer.stop();
+        objPlayer.release();
+    }
+
+    public void onPause(){
+        objPlayer.stop();
+        objPlayer.release();
+    }
+
+    public void onDestroy(){
+        objPlayer.stop();
+        objPlayer.release();
+    }
+
+    @Override
+    public IBinder onBind(Intent objIndent) {
+        return null;
+    }
 }
